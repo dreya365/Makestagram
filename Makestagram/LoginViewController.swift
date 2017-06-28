2b9843fa3fb55ad5af1e26c0ff09c820a589687a
dreya365 <dreya365@gmail.com>
2017-06-28 10:18:31 -0400

    init

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Andreya Wallang (Temporary) on 6/26/17.
+//  Copyright © 2017 Andreya Wallang (Temporary). All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigtaion
+ 
+    // In a storyboard-based application, you will often want to do a little preperation before navigation override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller 
+    }
+    */
+    
+}
